22d3a69c984f35d2c5b4256f81ca86320d2d9885
dhiaan2614 <dhiaan.rupani@gmail.com>
2026-03-17 16:44:37 +0530

    Add files via upload

---
+++ cw21.py
@@ -0,0 +1,16 @@
+a=10
+b=5
+c=10
+
+
+avg=(a+b+c)/3
+
+print(avg)
+
+
+if a<avg:
+    print('A is riding slower than the avg speed')
+elif b<avg:
+    print('b is riding slower than the avg speed')
+elif c<avg:
+    print('c is riding slower than the avg speed')
