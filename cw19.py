f01fe82ccf0f51f632d5eeda91d51a9073b07d03
dhiaan2614 <dhiaan.rupani@gmail.com>
2026-03-17 16:31:11 +0530

    Add files via upload

---
+++ cw19.py
@@ -0,0 +1,10 @@
+n=int(input('please enter a number: '))
+n2=int(input('please enter another number: '))
+
+r= n%n2
+ 
+if r == 0:
+    print('Both of these number are divisible by one another')
+else:
+    print('Both of these numbers are not divisible by one another')
+    
