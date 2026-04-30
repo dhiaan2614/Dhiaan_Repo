aeaa72f85398a686525657fca2d5d368f526a03c
dhiaan2614 <dhiaan.rupani@gmail.com>
2026-04-30 16:21:02 +0530

    Add files via upload

---
+++ cw49.py
@@ -0,0 +1,6 @@
+# Write a program to check alphabet “A” is present in the given string or not. And terminate the loop after finding the alphabet “A.
+str='abcd'
+for i in str:
+    print(i)
+    if i=='a':
+        break
