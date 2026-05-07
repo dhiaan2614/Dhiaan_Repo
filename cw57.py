f071c88a91897262a5a59325c179d72b7d974529
dhiaan2614 <dhiaan.rupani@gmail.com>
2026-05-07 16:17:51 +0530

    Add files via upload

---
+++ cw57.py
@@ -0,0 +1,12 @@
+import random
+b=(random.randint(0,10))
+
+a=int(input("Please enter a number between 1 - 10: "))
+
+if b==a:
+    print('Both numbers are the same!')
+    print(b)
+else:
+    print('Both numbers are not the same!')
+    print(b)
+    
