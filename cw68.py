58a13a5b6f128df23dc7b253261dafd23f9dd1b1
dhiaan2614 <dhiaan.rupani@gmail.com>
2026-05-28 16:44:32 +0530

    Add files via upload

---
+++ cw68.py
@@ -0,0 +1,15 @@
+import math
+mylist=[12,26,15]
+
+sum= math.fsum(mylist)
+print(sum)
+avg = sum/3
+print(avg)
+
+mylist.sort()
+print(mylist)
+print(mylist[0])
+print(mylist[len(mylist)-1])
+print(len(mylist))
+
+    
