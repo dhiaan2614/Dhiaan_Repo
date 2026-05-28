a36a26aa285d80d2b18a99e0529ddc3b3d725b45
dhiaan2614 <dhiaan.rupani@gmail.com>
2026-05-28 16:27:52 +0530

    Add files via upload

---
+++ cw67.py
@@ -0,0 +1,9 @@
+# Write a program to perform the following operations on a List: 1. Create an empty list 2. A list with elements 3. Use * operator 4. Reverse a list
+
+
+
+mylist=[12,26,27,28]
+mylist1=[]
+
+print(mylist*0)
+print(mylist[::-1])
