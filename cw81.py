fde79794de1bdeda84681a5bc23bd5b54157fcc0
dhiaan2614 <dhiaan.rupani@gmail.com>
2026-08-25 17:34:12 +0530

    Add files via upload

---
+++ cw81.py
@@ -0,0 +1,8 @@
+# Write a program to return the country code for various countries. Here’s a dictionary of different country codes - {'India' : '0091', 'Australia' : '0025', 'Nepal' : '00977'}.
+
+
+dic1 = {'India' : '0091', 'Australia' : '0025', 'Nepal' : '00977'}
+
+country = input('Which country code do you want?')
+
+print(dic1[country])
